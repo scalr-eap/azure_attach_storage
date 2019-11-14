ceec92e0d18f7778373157d4f0cf2b4bebf121b1
Ryan <ryan.fee625@gmail.com>
2019-11-14 14:28:39 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,13 +6,19 @@ variable "scalr_azurerm_client_secret" {}
 
 variable "scalr_azurerm_tenant_id" {}
 
-variable "storage_name" {}
+variable "storage_name" {
+  description = "Name of the storage device"
+ }
 
 variable "rg_name" {}
 
 variable "region" {}
 
 variable "rg" {}
+
+variable "size" {
+ description = "Size of the disk"
+ }
 
 variable "serverid" {
  description = "Must be full path" 
