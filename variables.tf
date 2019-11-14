7a898d21d10983ee5d4a5a1c570bcf20481c99e6
Ryan <ryan.fee625@gmail.com>
2019-11-14 14:25:25 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,3 +5,15 @@ variable "scalr_azurerm_client_id" {}
 variable "scalr_azurerm_client_secret" {}
 
 variable "scalr_azurerm_tenant_id" {}
+
+variable "storage_name" {}
+
+variable "rg_name" {}
+
+variable "region" {}
+
+variable "rg" {}
+
+variable "serverid" {
+ description = "Must be full path" 
+  }
