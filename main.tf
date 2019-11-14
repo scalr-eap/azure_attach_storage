a2c035921b85cbc62687c4576101f538c1d854e5
Ryan <ryan.fee625@gmail.com>
2019-11-14 14:20:39 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,17 +6,17 @@ provider "azurerm" {
 }
 
 resource "azurerm_managed_disk" "test" {
-  name                 = "azurestorage3"
-  location             = "East US"
-  resource_group_name  = "scalr-demo"
+  name                 = var.storage_name
+  location             = var.region
+  resource_group_name  = var.rg
   storage_account_type = "Standard_LRS"
   create_option        = "Empty"
-  disk_size_gb         = 1
+  disk_size_gb         = var.size
 }
 
 resource "azurerm_virtual_machine_data_disk_attachment" "test" {
   managed_disk_id    = "${azurerm_managed_disk.test.id}"
-  virtual_machine_id = "/subscriptions/1482f7c9-0cf4-43f6-be8b-5971fb3eb4d1/resourceGroups/scalr-demo/providers/Microsoft.Compute/virtualMachines/azurestorage2"
+  virtual_machine_id = var.serverid
   lun                = "1"
   caching            = "ReadWrite"
 }
